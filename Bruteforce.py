d7abdac04223a1af56226f4d49431a6fa948f57e
FortuneMonkey <saputraa.sp.20@gmail.com>
2024-03-18 23:16:19 +0800

    First Commit
    First Date: 18-03-2024

---
+++ Bruteforce.py
@@ -0,0 +1,21 @@
+from random import *
+import os
+
+u_pwd=input("Enter a Password : ")
+pwd=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+     '1','2','3','4','5','6','7','8','9','0']
+
+pw=""
+count=0
+while(pw!=u_pwd):
+    pw=""
+    for letter in range (len(u_pwd)):
+        guess_pwd=pwd[randint(0,35)]
+        pw=str(guess_pwd)+str(pw)
+        print(pw)
+        print("Cracking Password... Please Wait...")
+        os.system("cls")
+        count+=1
+
+print("Your Password Is : ",pw)
+print("Total Attempt    : ",count)
